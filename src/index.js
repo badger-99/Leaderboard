857b6a44c8b3b9ec8535f3557ae31e33758e9dd5
Alfred Peter <alpeter91@gmail.com>
2023-06-28 17:44:29 +0300

    Add API key

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './style.css';
 const refresh = document.getElementById('refresh');
 const form = document.getElementById('form');
 const scoreList = document.getElementById('score-list');
+const apiKey = '5ZsB1os0MNFSzXzByfC2';
