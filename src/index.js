5ea42a24edddb1fa3e7663dbb3ce1bdf59784502
Alfred Peter <alpeter91@gmail.com>
2023-06-28 17:45:02 +0300

    Add API url

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const refresh = document.getElementById('refresh');
 const form = document.getElementById('form');
 const scoreList = document.getElementById('score-list');
 const apiKey = '5ZsB1os0MNFSzXzByfC2';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`;
+
